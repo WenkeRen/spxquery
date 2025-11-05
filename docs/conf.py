9381489ca819fe394d6bf954b23fa385719f4ace
Wenke <rwk13783243980@live.com>
2025-11-05 15:44:20 +0800

    Bump version to v0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -5,46 +5,48 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+
+sys.path.insert(0, os.path.abspath("../src"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'SPXQuery'
-copyright = '2025, Wenke Ren'
-author = 'Wenke Ren'
+project = "SPXQuery"
+copyright = "2025, Wenke Ren"
+author = "Wenke Ren"
 
 # Get version from package
 try:
     from spxquery import __version__
+
     release = __version__
-    version = '.'.join(release.split('.')[:2])  # Short X.Y version
+    version = ".".join(release.split(".")[:2])  # Short X.Y version
 except ImportError:
     # Fallback if package not installed
-    release = '0.2.0'
-    version = '0.2'
+    release = "0.2.0"
+    version = "0.2"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',  # Markdown support
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'nbsphinx',
+    "myst_parser",  # Markdown support
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "nbsphinx",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 # -- Extension configuration -------------------------------------------------
 
@@ -66,11 +68,11 @@ napoleon_attr_annotations = True
 
 # Autodoc settings
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
 }
 
 # Autosummary settings
@@ -78,29 +80,29 @@ autosummary_generate = True
 
 # Intersphinx mapping to external documentation
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'astropy': ('https://docs.astropy.org/en/stable/', None),
-    'matplotlib': ('https://matplotlib.org/stable/', None),
-    'pandas': ('https://pandas.pydata.org/docs/', None),
-    'photutils': ('https://photutils.readthedocs.io/en/stable/', None),
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "astropy": ("https://docs.astropy.org/en/stable/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+    "photutils": ("https://photutils.readthedocs.io/en/stable/", None),
 }
 
 # nbsphinx settings
-nbsphinx_execute = 'never'  # Don't execute notebooks during build
+nbsphinx_execute = "never"  # Don't execute notebooks during build
 nbsphinx_allow_errors = True
 
 # MyST parser settings
 myst_enable_extensions = [
-    "colon_fence",      # ::: fences for directives
-    "deflist",          # Definition lists
-    "fieldlist",        # Field lists
-    "html_image",       # Raw HTML images
-    "replacements",     # Text replacements
-    "smartquotes",      # Smart quotes
-    "tasklist",         # Task lists
+    "colon_fence",  # ::: fences for directives
+    "deflist",  # Definition lists
+    "fieldlist",  # Field lists
+    "html_image",  # Raw HTML images
+    "replacements",  # Text replacements
+    "smartquotes",  # Smart quotes
+    "tasklist",  # Task lists
 ]
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
